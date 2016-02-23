ebecb37f46efcf9651f3334744007e06ad95e5d4
Piot Liszka <piotr.liszk.pl@gmail.com>
2016-02-23 20:50:37 +0100

    Powiększono meni dla szkoleń i tutoriali.

---
+++ example/src/main/java/pl/maniak/appexample/fragment/MainAdvancedTutorialFragment.java
@@ -0,0 +1,25 @@
+package pl.maniak.appexample.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import pl.maniak.appexample.R;
+import pl.maniak.appexample.common.log.L;
+
+/**
+ * Created by Maniak on 2015-09-29.
+ */
+public class MainAdvancedTutorialFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        L.i("MainGoogleFragment.onCreateView() ");
+        ViewGroup root = (ViewGroup)inflater.inflate(R.layout.fragment_advanced_tutorial_main, null);
+
+        return root;
+    }
+}
---
+++ example/src/main/java/pl/maniak/appexample/fragment/MainClaverPointFragment.java
@@ -0,0 +1,23 @@
+package pl.maniak.appexample.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import pl.maniak.appexample.R;
+import pl.maniak.appexample.common.log.L;
+
+/**
+ * Created by maniak on 23.02.16.
+ */
+public class MainClaverPointFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        L.i("MainSecurityFragment.onCreateView() ");
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_claver_point_main, null);
+        return root;
+    }
+}
